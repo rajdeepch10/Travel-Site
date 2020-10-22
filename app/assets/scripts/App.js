4fb5bd6d34761d7a8b94b1ec569ca5fa5196822d
Rajdeep <rajdeepch0@gmail.com>
2020-10-23 03:29:26 +1100

    Example JavaScript

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,17 @@ import '../styles/styles.css';
 if (module.hot) {
 	module.hot.accept();
 }
+
+/* Lesson example code*/
+function Person(name, favColor) {
+	(this.name = name), (this.favColor = favColor);
+	this.greet = function() {
+		console.log('Hello There! ' + this.name + ' Color: ' + this.favColor + ' .');
+	};
+}
+
+let john = new Person('John Doe', 'Yellow');
+john.greet();
+
+var jane = new Person('Jane Smith', 'Blue');
+jane.greet();
