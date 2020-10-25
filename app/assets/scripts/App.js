c9601690a629547664ddcc2602321184347c3349
Rajdeep <rajdeepch0@gmail.com>
2020-10-25 23:11:21 +1100

    Completed Header with JavaScript

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,9 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
 	module.hot.accept();
 }
 
-/* Lesson example code*/
-function Person(name, favColor) {
-	(this.name = name), (this.favColor = favColor);
-	this.greet = function() {
-		console.log('Hello There! ' + this.name + ' Color: ' + this.favColor + ' .');
-	};
-}
-
-let john = new Person('John Doe', 'Yellow');
-john.greet();
-
-var jane = new Person('Jane Smith', 'Blue');
-jane.greet();
